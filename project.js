944f71a7a9e13942e2bed0a67569a87bce389712
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-26 00:17:25 +0600

    my bank account using javascript

--- project.js
+++ project.js
@@ -1,3 +1,5 @@
+//login button event handler
+
 const loginButton= document.getElementById('login');
 loginButton.addEventListener("click", function(){
    const loginArea = document.getElementById('login-area');
@@ -5,5 +7,52 @@ loginButton.addEventListener("click", function(){
 
    const transactionArea = document.getElementById('transaction-area');
    transactionArea.style.display='block';
+
+});
+
+//deposit handler
+
+const depositBtn = document.getElementById("deposit");
+depositBtn.addEventListener("click", function (){
+            const depositAmount = document.getElementById("depositAmount").value;
+            const depositNumber = parseFloat(depositAmount);
+            // console.log(depositNumber);
+            const currentAmount = document.getElementById("currentDeposit").innerText;
+            const currentNumber = parseFloat(currentAmount);
+            const totalDeposit = depositNumber + currentNumber;
+            document.getElementById("currentDeposit").innerText = totalDeposit;
+            
+            // current Balance
+            const currentSavings = document.getElementById("currentBalance").innerText;
+            const depositNumbers = parseFloat(currentSavings);
+            const totalBalance = depositNumber + depositNumbers;
+            document.getElementById("currentBalance").innerText = totalBalance;
+
+            document.getElementById("depositAmount").value ='';
+
+            
+
+});
+
+
+const withdrawBtn= document.getElementById('addWithdraw');
+withdrawBtn.addEventListener("click", function(){
+
+//withdraw balance
+
+const withdrawAmount = document.getElementById("withdrawAmount").value;
+const withdrawNumber = parseFloat(withdrawAmount);
+const lastWithdraws = document.getElementById("lastWithdraw").innerText;
+const currentWithdraws = parseFloat(lastWithdraws);
+const totalWithdraws = withdrawNumber + currentWithdraws;
+document.getElementById("lastWithdraw").innerText = totalWithdraws;
+
+const currentSavings = document.getElementById("currentBalance").innerText;
+            const depositNumbers = parseFloat(currentSavings);
+            document.getElementById("currentBalance").innerText = depositNumbers - withdrawNumber;
+
+
+document.getElementById("withdrawAmount").value ='';
+
 
 });
